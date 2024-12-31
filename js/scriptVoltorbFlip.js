52244da477e02903152773090e495ec895d0c94a
Nico <nicograf888@gmail.com>
2024-12-31 16:36:52 +0100

    Add decorative rows to layout

--- js/scriptVoltorbFlip.js
+++ js/scriptVoltorbFlip.js
@@ -1,3 +1,6 @@
+//Support variables for color scheme
+const colorScheme = ["Red", "Green", "Yellow", "Blue", "Purple"];
+
 function createCardLayout() {
 
     //Get id of div where cards are placed
@@ -8,29 +11,41 @@ function createCardLayout() {
     //Append both to cardArea
     for (let i = 0; i < 5; i++) {
         for (let j = 0; j < 5; j++) {
-            const cardBackground = document.createElement("div");
-            cardBackground.className = "cardBackground";
-
-            const card = document.createElement("div");
-            card.className = "card";
-            card.id = i * 5 + j;
-
-            cardBackground.appendChild(card);
-            cardArea.appendChild(cardBackground);
+            createCardRow(i, j, cardArea, colorScheme);
         }
-        createInfoBox("Row", i, cardArea);
+        createInfoBox("Row", i, cardArea, colorScheme);
     }
 
     //Create additional 6th row with column info tiles
     for (let i = 0; i < 5; i++) {
-        createInfoBox("Column", i, cardArea);
+        createInfoBox("Column", i, cardArea, colorScheme);
     }
 }
 
-function createInfoBox(rowOrColumn, index, container) {
+function createCardRow(rowIndex, columnIndex, container, colorScheme) {
 
-    //Support variables for row/column info
-    const colorScheme = ["Red", "Green", "Yellow", "Blue", "Purple"];
+    const cardBackground = document.createElement("div");
+    cardBackground.className = "cardBackground";
+
+    const card = document.createElement("div");
+    card.className = "card";
+    card.id = rowIndex * 5 + columnIndex;
+
+    cardBackground.appendChild(card);
+    container.appendChild(cardBackground);
+
+    const coloredLineBackground = document.createElement("div");
+    coloredLineBackground.className = "coloredLineBackground";
+
+    const coloredLine = document.createElement("div");
+    coloredLine.className = "coloredLineHorizontal"
+    coloredLine.id = "coloredLineHorizontal" + colorScheme[rowIndex];
+    
+    coloredLineBackground.appendChild(coloredLine);
+    container.appendChild(coloredLineBackground);
+}
+
+function createInfoBox(rowOrColumn, index, container, colorScheme) {
 
     //Create div background for columnInfo
     const infoBoxBackground = document.createElement("div");
@@ -77,4 +92,17 @@ function createInfoBox(rowOrColumn, index, container) {
     //Append columnInfo to background, append card to cardArea
     infoBoxBackground.appendChild(infoBox);
     container.appendChild(infoBoxBackground);
+
+    if(rowOrColumn === "Column"){
+        const coloredLineBackground = document.createElement("div");
+        coloredLineBackground.className = "coloredLineBackgroundInfoBox";
+
+        const coloredLine = document.createElement("div");
+        coloredLine.className = "coloredLineHorizontal"
+        coloredLine.id = "coloredLineHorizontalInfoBox";
+
+        coloredLineBackground.appendChild(coloredLine);
+        container.appendChild(coloredLineBackground);
+    }
+
 }
